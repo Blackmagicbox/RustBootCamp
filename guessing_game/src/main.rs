7ae7ec5b4b03c9c18226a0e088b56221fbc2ba65
Rafael Santos <rafa@blackmagicbox.com.br>
2023-12-16 01:53:53 +0100

    Feat Comparison

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+
 use std::io;
 
 fn main() {
@@ -16,5 +17,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        std::cmp::Ordering::Less => println!("Too small!"),
+        std::cmp::Ordering::Greater => println!("Too big!"),
+        std::cmp::Ordering::Equal => println!("You win!"),
+    }
 }
