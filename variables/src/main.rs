f546e8ff3e465cc21dd80dd94dc8bf1bce084843
Rafael Santos <rafa@blackmagicbox.com.br>
2023-12-17 01:27:22 +0100

    Move constants to the global scope

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,7 @@
+const STARTING_MISSILES: i32 = 8;
+const READY_AMOUNT: i32 = 2;
+
 fn main() {
-    const STARTING_MISSILES: i32 = 8;
-    const READY_AMOUNT: i32 = 2;
     let missiles = STARTING_MISSILES;
     let ready = READY_AMOUNT;
 
