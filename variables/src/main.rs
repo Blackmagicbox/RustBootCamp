822d820548c9c9c062b907e22265420ba953b13f
Rafael Santos <rafa@blackmagicbox.com.br>
2023-12-17 01:26:15 +0100

    Move initial values to consts

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
-    let missiles: u32 = 8;
-    let ready = 2;
+    const STARTING_MISSILES: i32 = 8;
+    const READY_AMOUNT: i32 = 2;
+    let missiles = STARTING_MISSILES;
+    let ready = READY_AMOUNT;
 
     println!("Firing {ready} of my {missiles} missiles...");
+
+    let missiles = missiles - ready;
+    println!("{missiles} missiles left");
 }
