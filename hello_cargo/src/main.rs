012e218814b84957db50650d912c5aa728de8874
Rafael Santos <rafa@blackmagicbox.com.br>
2023-12-19 00:53:29 +0100

    Feat SquareRoot Test

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,3 +1,10 @@
+fn square_root(x: f64) -> f64 {
+    x.sqrt()
+}
+
 fn main() {
-    println!("Hello, cargo!");
+    println!(
+        "Hello, cargo! {square_root}",
+        square_root = square_root(9.0)
+    );
 }
