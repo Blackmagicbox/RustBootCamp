e6382a1239b949d3d62a30c391e68a6aedc7e109
Rafael Santos <rafa@blackmagicbox.com.br>
2023-12-22 22:51:13 +0100

    Feat Date

--- twelve_days_of_christmas/src/main.rs
+++ twelve_days_of_christmas/src/main.rs
@@ -1,6 +1,8 @@
 use twelve_days_of_christmas::sing;
 
 fn main() {
-    println!("let's sing a song!\nThe twelve days of Christmas\n");
+    let date = chrono::Local::now();
+    println!("Today is {}", date.format("%A, %B %e, %Y"));
+    println!("Let's sing a song!\nThe twelve days of Christmas\n");
     sing();
 }
