ef7683fdf1cadc58ccba2284aa3c231451324402
Rafael Santos <rafa@blackmagicbox.com.br>
2023-12-23 00:18:29 +0100

    Test double borrowing mutable reference

--- twelve_days_of_christmas/src/main.rs
+++ twelve_days_of_christmas/src/main.rs
@@ -1,8 +1,11 @@
-use twelve_days_of_christmas::sing;
+use twelve_days_of_christmas::{sing, try_to_reference_two_variables};
 
 fn main() {
     let date = chrono::Local::now();
     println!("Today is {}", date.format("%A, %B %e, %Y"));
     println!("Let's sing a song!\nThe twelve days of Christmas\n");
     sing();
+
+    let s = try_to_reference_two_variables();
+    println!("{s}");
 }
